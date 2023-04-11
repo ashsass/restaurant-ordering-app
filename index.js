b9cf37d9a2003ddb03760a7b486b427c1ae93c2c
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-04-11 16:51:58 -0400

    Fixed more spacing issues in the menu html and worked on hr styling

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ function getMenuHtml() {
 
     menuArray.forEach(item => {
         menuHtml += `
-        <div class="item-${item.name}">
-                <div class="${item.name}-emoji">${item.emoji}</div>
-                <div class="${item.name}-text">
-                    <h4>${item.name}</h4>
-                    <p>${item.ingredients}</p>
+        <div class="item">
+                <div class="item-emoji" id="${item.name}-emoji">${item.emoji}</div>
+                <div class="item-text" id="${item.name}-text">
+                    <h3>${item.name}</h3>
+                    <p class="ingredients">${item.ingredients.join(', ')}</p>
                     <p>${item.price}</p>
                 </div>
                 <i class="fa-light fa-plus"></i>
-            </div>`
+            </div>
+            <hr>`
     })
     return menuHtml
 }
