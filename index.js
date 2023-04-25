174b49b7bcb9470fc0755a3078f0b831a8d6eca9
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-04-25 14:41:34 -0400

    Finalize the default styling

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ function getMenuHtml() {
                 <div class="item-emoji" id="${item.name}-emoji">${item.emoji}</div>
                 <div class="item-text" id="${item.name}-text">
                     <h3>${item.name}</h3>
-                    <p class="ingredients">${item.ingredients.join(', ')}</p>
-                    <p>${item.price}</p>
+                    <p class="item-ingredients">${item.ingredients.join(', ')}</p>
+                    <p class="item-price">${item.price}</p>
                 </div>
-                <i class="fa-light fa-plus"></i>
+                <button class="item-add">
+                    <i class="fa-light fa-plus"></i>
+                </button>
+                <hr>
             </div>
-            <hr>`
+            `
     })
     return menuHtml
 }
