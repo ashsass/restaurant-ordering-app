ab8b3fbdf030e3e7a530fc2f12b118c3a8e4a140
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-04-29 10:58:29 -0400

    Render the order when items are added. Need to have counts change when item is selected multiple times

--- data.js
+++ data.js
@@ -4,20 +4,23 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushroom", "mozzarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        quantity: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        quantity: 0
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        quantity: 0
     }
 ]
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Add a feature so that on click the initial input value clears so the customer can input their information with ease
 
-import { menuArray } from "./data.js";
+import { menuArray } from "./data.js";  
+const orderArray = []
 
 document.addEventListener('click', function(e){
     if(e.target.dataset.item){
@@ -28,8 +29,25 @@ function getMenuHtml() {
     return menuHtml
 }
 
-function getOrderHtml() {
-
+function getOrderHtml(id) {
+    let orderHtml = ``
+    //check if the array has the corresponding item id and if not push it to the array
+    if (!orderArray.includes(id)){
+        orderArray.push(id)
+    
+        //iterate over the array and pull the id that is present to render the order html. This will allow us to exlude multiple orders. Will need to update a count in someway
+        orderArray.forEach(id => {
+            orderHtml +=
+                `<div class="order-item-${menuArray[id].name}" id="${id}">
+                    <h3 class="order-name">${menuArray[id].name}</h3>
+                    <button class="remove">remove</button>
+                    <p class="quantity">1</p>
+                    <p class="item-price order-item-price">$${menuArray[id].price}</p>
+                </div>`
+ 
+         return document.querySelector('.order-items-container').innerHTML = orderHtml
+        })
+    }
 }
 
 function render() {
